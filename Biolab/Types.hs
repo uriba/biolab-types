397659f7eae7c4b5c589249017c065696d8ee22c
Uri Barenholz <uri.barenholz@gmail.com>
2012-12-23 11:45:51 +0200

    initial commit

---
+++ Biolab/Types.hs
@@ -0,0 +1,39 @@
+module Biolab.Types (
+    Well(..),
+    Measurement(..),
+    SampleId(..),
+    LabeledData,
+    )
+where
+import Data.DateTime (DateTime)
+import qualified Data.Map as M
+import Data.Map (Map)
+
+data SampleId = SampleId { sidExpId :: String, sidPlate :: Int, sidWell :: Well} deriving (Eq, Ord, Show, Read)
+
+data Well = Well {
+        wRow :: Char,
+        wColumn :: Int
+    } deriving (Eq, Show, Read, Ord)
+
+wellStr :: Well -> String
+wellStr w = concat ["(", [wRow w],",",show . wColumn $ w,")"]
+
+data Measurement = Measurement {
+        mExpDesc :: String,
+        mPlate :: Int,
+        mTime :: DateTime,
+        mType :: String,
+        mWell :: Well,
+        mVal :: Double
+    } deriving (Eq, Show)
+
+type LabeledData a = Map String (Map SampleId a)
+    
+ldMap :: (a -> b) -> LabeledData a -> LabeledData b
+ldMap f = M.map (M.map f)
+
+ldZip :: (a -> b -> c) -> LabeledData a -> LabeledData b -> LabeledData c
+ldZip f = M.intersectionWith (M.intersectionWith f)
+
+type ExpData = LabeledData [Measurement]
