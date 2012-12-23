76890ad3d252ae4c47d8f113f64b1dd676e77917
Uri Barenholz <uri.barenholz@gmail.com>
2012-12-23 16:22:55 +0200

    added a few necessary types, first integration with biolab-interfaces

--- Biolab/Types.hs
+++ Biolab/Types.hs
@@ -1,17 +1,23 @@
 module Biolab.Types (
     Well(..),
-    Measurement(..),
     SampleId(..),
     LabeledData,
     MesType(..),
+    RawMeasurement(..),
     ExpData,
+    wellStr,
+    ColonySample,
     )
 where
 import Data.DateTime (DateTime)
 import qualified Data.Map as M
+import Data.Vector (Vector)
 import Data.Map (Map)
 
+newtype RawMeasurement = RawMeasurement {mVal :: Double} deriving (Eq, Ord)
 data SampleId = SampleId { sidExpId :: String, sidPlate :: Int, sidWell :: Well} deriving (Eq, Ord, Show, Read)
+data MesType = Absorbance Int | Fluorescence Int Int | Luminesense Int deriving (Eq, Ord, Show, Read)
+type ColonySample = Vector(DateTime, RawMeasurement)
 
 data Well = Well {
         wRow :: Char,
@@ -21,17 +27,6 @@ data Well = Well {
 wellStr :: Well -> String
 wellStr w = concat ["(", [wRow w],",",show . wColumn $ w,")"]
 
-data MesType = Absorbance Int | Fluorescence Int Int | Luminesense Int
-
-data Measurement = Measurement {
-        mExpDesc :: String,
-        mPlate :: Int,
-        mTime :: DateTime,
-        mType :: String,
-        mWell :: Well,
-        mVal :: Double
-    } deriving (Eq, Show)
-
 type LabeledData a = Map String (Map SampleId a)
     
 ldMap :: (a -> b) -> LabeledData a -> LabeledData b
@@ -40,4 +35,4 @@ ldMap f = M.map (M.map f)
 ldZip :: (a -> b -> c) -> LabeledData a -> LabeledData b -> LabeledData c
 ldZip f = M.intersectionWith (M.intersectionWith f)
 
-type ExpData = LabeledData [Measurement]
+type ExpData = LabeledData [(MesType,ColonySample)]
