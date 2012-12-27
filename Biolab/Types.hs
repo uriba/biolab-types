9179434d0f81ed78a5e07626866022e7f8eba894
Uri Barenholz <uri.barenholz@gmail.com>
2012-12-27 16:55:29 +0200

    ColonySample class introduced, added types for different measurement types of data

--- Biolab/Types.hs
+++ Biolab/Types.hs
@@ -4,9 +4,17 @@ module Biolab.Types (
     LabeledData,
     MesType(..),
     RawMeasurement(..),
+    NormalizedMeasurement(..),
     ExpData,
     wellStr,
     ColonySample,
+    AbsorbanceSample(..),
+    FluorescenseSample(..),
+    LuminescenseSample(..),
+    RawColonyMeasurements,
+    RawAbsorbance(..),
+    RawFluorescence(..),
+    RawLuminescense(..),
     )
 where
 import Data.DateTime (DateTime)
@@ -15,9 +23,32 @@ import Data.Vector (Vector)
 import Data.Map (Map)
 
 newtype RawMeasurement = RawMeasurement {mVal :: Double} deriving (Eq, Ord, Show)
+newtype NormalizedMeasurement = NormalizedMeasurement {nmVal :: Double}
+
 data SampleId = SampleId { sidExpId :: String, sidPlate :: Int, sidWell :: Well} deriving (Eq, Ord, Show, Read)
 data MesType = Absorbance Int | Fluorescence Int Int | Luminesense Int deriving (Eq, Ord, Show, Read)
-type ColonySample = Vector(DateTime, RawMeasurement)
+
+type ColonyMeasurements a = Vector (DateTime, a)
+type RawColonyMeasurements = ColonyMeasurements RawMeasurement
+type NormalizedColonyMeasurements = ColonyMeasurements NormalizedMeasurement
+
+class ColonySample a where
+    measurements :: a b -> ColonyMeasurements b
+
+data AbsorbanceSample a = AbsorbanceSample { asWaveLength :: Int, asMes :: ColonyMeasurements a }
+type RawAbsorbance = AbsorbanceSample RawMeasurement
+instance ColonySample AbsorbanceSample where
+    measurements = asMes
+
+data FluorescenseSample a = FluorescenseSample { flExcitation :: Int, flEmission :: Int, flMes :: ColonyMeasurements a }
+type RawFluorescence = FluorescenseSample RawMeasurement
+instance ColonySample FluorescenseSample where
+    measurements = flMes
+
+data LuminescenseSample a = LuminescenseSample { lsWaveLength :: Int, lsMes :: ColonyMeasurements a }
+type RawLuminescense = LuminescenseSample RawMeasurement
+instance ColonySample LuminescenseSample where
+    measurements = lsMes
 
 data Well = Well {
         wRow :: Char,
@@ -35,4 +66,4 @@ ldMap f = M.map (M.map f)
 ldZip :: (a -> b -> c) -> LabeledData a -> LabeledData b -> LabeledData c
 ldZip f = M.intersectionWith (M.intersectionWith f)
 
-type ExpData = LabeledData [(MesType,ColonySample)]
+type ExpData = LabeledData ([RawAbsorbance],[RawFluorescence],[RawLuminescense])
