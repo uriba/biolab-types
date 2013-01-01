a622aad1ea93f8a5f8a505fe1500275917f650fe
Uri Barenholz <uri.barenholz@gmail.com>
2013-01-01 11:30:38 +0200

    some refactring for a cleaner representation of different measurement kinds (fluorescence vs absorbance vs luminescense)

--- Biolab/Types.hs
+++ Biolab/Types.hs
@@ -12,6 +12,7 @@ module Biolab.Types (
     FluorescenseSample(..),
     LuminescenseSample(..),
     RawColonyMeasurements,
+    ColonyMeasurements(..),
     RawAbsorbance(..),
     RawFluorescence(..),
     RawLuminescense(..),
@@ -31,34 +32,38 @@ newtype NormalizedMeasurement = NormalizedMeasurement {nmVal :: Double}
 data SampleId = SampleId { sidExpId :: String, sidPlate :: Int, sidWell :: Well} deriving (Eq, Ord, Show, Read)
 data MesType = Absorbance Int | Fluorescence Int Int | Luminesense Int deriving (Eq, Ord, Show, Read)
 
-type ColonyMeasurements a = Vector (DateTime, a)
-type RawColonyMeasurements = ColonyMeasurements RawMeasurement
-type NormalizedColonyMeasurements = ColonyMeasurements NormalizedMeasurement
+type ColonyMeasurementsData a = Vector (DateTime, a)
+type RawColonyMeasurements = ColonyMeasurementsData RawMeasurement
+type NormalizedColonyMeasurements = ColonyMeasurementsData NormalizedMeasurement
 
 class ColonySample a where
-    measurements :: a b -> ColonyMeasurements b
-    process :: (ColonyMeasurements b -> ColonyMeasurements c) -> a b -> a c
+    measurements :: a b -> ColonyMeasurementsData b
+    process :: (ColonyMeasurementsData b -> ColonyMeasurementsData c) -> a b -> a c
 
-data AbsorbanceSample a = AbsorbanceSample { asWaveLength :: Int, asMes :: ColonyMeasurements a }
+data AbsorbanceSample a = AbsorbanceSample { asWaveLength :: Int, asMes :: ColonyMeasurementsData a }
 type RawAbsorbance = AbsorbanceSample RawMeasurement
 type NormalizedAbsorbance = AbsorbanceSample NormalizedMeasurement
 instance ColonySample AbsorbanceSample where
     measurements = asMes
     process f as = as {asMes = f . asMes $ as}
 
-data FluorescenseSample a = FluorescenseSample { flExcitation :: Int, flEmission :: Int, flMes :: ColonyMeasurements a }
+data FluorescenseSample a = FluorescenseSample { flExcitation :: Int, flEmission :: Int, flMes :: ColonyMeasurementsData a }
 type RawFluorescence = FluorescenseSample RawMeasurement
 type NormalizedFluorescence = FluorescenseSample NormalizedMeasurement
 instance ColonySample FluorescenseSample where
     measurements = flMes
     process f fl = fl {flMes = f . flMes $ fl}
 
-data LuminescenseSample a = LuminescenseSample { lsWaveLength :: Int, lsMes :: ColonyMeasurements a }
+data LuminescenseSample a = LuminescenseSample { lsWaveLength :: Int, lsMes :: ColonyMeasurementsData a }
 type RawLuminescense = LuminescenseSample RawMeasurement
 type NormalizedLuminescense = LuminescenseSample NormalizedMeasurement
 instance ColonySample LuminescenseSample where
     measurements = lsMes
     process f lum = lum {lsMes = f . lsMes $ lum}
+
+data ColonyMeasurements a   = AbsorbanceMeasurement (AbsorbanceSample a)
+                            | FluorescenseMeasurement (FluorescenseSample a)
+                            | LuminesenseMeasurement (LuminescenseSample a)
 
 data Well = Well {
         wRow :: Char,
@@ -76,4 +81,4 @@ ldMap f = M.map (M.map f)
 ldZip :: (a -> b -> c) -> LabeledData a -> LabeledData b -> LabeledData c
 ldZip f = M.intersectionWith (M.intersectionWith f)
 
-type ExpData = LabeledData ([RawAbsorbance],[RawFluorescence],[RawLuminescense])
+type ExpData = LabeledData ([ColonyMeasurements RawMeasurement])
